94fd57df3b9f0adeba218e8d23f7034b5b72cf70
petert360 <peter.t360@hotmail.com>
2021-09-09 00:12:26 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,10 +5,17 @@ import { Observable } from 'rxjs';
 
 let observable = new Observable((observer: any) => {
   observer.next('Hi!');
+  observer.next('How are you?');
+  observer.complete();
 });
 
-observable.subscribe((x: any) => addItem(x));
+observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem('Completed')
+);
 
+// ez a fv. jelenyíti meg JS kóddal a li elemet
 function addItem(val: any) {
   let node = document.createElement('li');
   let textnode = document.createTextNode(val);
